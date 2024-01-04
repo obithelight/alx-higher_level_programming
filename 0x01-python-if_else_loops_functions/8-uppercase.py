d099d0ee1d98a5286463567df6e9794e4d32107a
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-04 10:15:09 +0800

    isupper function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    upper = ""
     for element in str:
-        if ord(element) <= 122 and ord(element) >= 97:
-            upper = (chr(ord(element) - 32))
-        print(f"{}".format(upper), end='')
+        if ord(element) >= 97 and ord(element) <= 122:
+            upper = chr(ord(element) - 32)
+        else:
+            upper = element
+        print("{}".format(upper), end='')
     print()
