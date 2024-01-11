f6705b1fa437124ea4493e97711a988a6e115cac
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-11 10:36:16 +0800

    function that returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        biggest_value_key = max(a_dictionary, key=a_dictionary.get) 
+        return biggest_value_key
+    else:
+        return None
