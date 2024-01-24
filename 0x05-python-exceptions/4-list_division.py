35cf1a0560692e545703305604c7971213a97290
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-25 01:03:26 +0800

    list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+
+    mylist = []
+
+    for item in range(list_length):
+        try:
+            answer = my_list_1[item] / my_list_2[item]
+
+        except TypeError:
+            print("wrong type")
+            answer = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            answer = 0
+        except IndexError:
+            print("out of range")
+            answer = 0
+        finally:
+            return mylist.append(answer)
+
+    return mylist
