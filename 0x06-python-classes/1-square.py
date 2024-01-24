28ba607762ba84f2a43a1e1418d14cc4e3be7573
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-24 10:55:39 +0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for the class Square"""
+
+class Square:
+    """Defines a square based on 0-square.py"""
+    def __init__(self, size):
+        """
+        Args:
+            size: length of a side of the square
+        """
+        self.__size = size
