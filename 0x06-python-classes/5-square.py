130bbb43e904613f650e6d011242f9cfd252e75b
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-25 10:58:50 +0800

    check task

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A module for Square Class"""
+
+
+class Square:
+    """Defines the Square Class"""
+    def __init__(self, size=0):
+        """
+        Initializes the class
+        Args:
+            size (int) - private instance attribute
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets and sets the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        result = self.__size ** 2
+        return result
+
+    def my_print(self):
+        for i in range(0, self.__size):
+            print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
