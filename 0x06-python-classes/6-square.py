52d85d8febf6e275faead51469fecbcd70141ca6
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-28 10:00:34 +0800

    cordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,8 +46,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
