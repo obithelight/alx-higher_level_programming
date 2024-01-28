ca4c82e65f5d1a7c07bae5a2757f71ebe1c6e8d8
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-28 09:55:11 +0800

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,6 +36,7 @@ class Square:
             or not all(isinstance(item, int) for item in value) \
                 or not all(item >= 0 for item in value):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         result = self.__size ** 2
