7af3c2b96de3597d84a2ad34a35715bc498cd804
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-06 06:19:29 +0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Represents a Geometry Module '''
+
+
+class BaseGeometry:
+    ''' Defines a Geometry Class '''
+
+    def area(self):
+        ''' Public Instance Method of Geometry Class '''
+
+        raise Exception("area() is not implemented")
