e88ac3ad4b5b2a8c7d9a5f887ce5f975477e0dae
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-06 04:03:10 +0800

    a class that inherits from a list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' A Python Module for Inheritance '''
+
+
+class MyList(list):
+    ''' This class inherits from another function '''
+
+    def print_sorted(self):
+        ''' Defines and prints the list '''
+
+        sorted_list = sorted(self)
+        print (sorted_list)
