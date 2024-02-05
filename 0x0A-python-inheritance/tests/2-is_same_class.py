0755bb886269cbe3b746e8f1cdf5a6650bbfb10f
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-06 05:05:23 +0800

    exact same object

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' A Python Module for Inheritance '''
+
+
+def is_same_class(obj, a_class):
+    ''' This function returns True for Exact Same Objects '''
+    return type(obj) == a_class
