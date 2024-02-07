79d4619e12beb1322cde5c450f5080755faddff6
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-08 06:07:49 +0800

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    A script that adds all arguments to a Python list
+    and saves them  to a JSON file
+'''
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    f = 'add_item.json'
+
+    # Check if the file exists, if not create an empty list
+    try:
+        items = load_from_json_file(f)
+    except FileNotFoundError:
+        items = []
+
+    # Add the new arguments to the list
+    items.extend(sys.argv[1:])
+
+    # Save the updated list to the file
+    save_to_json_file(items, f)
