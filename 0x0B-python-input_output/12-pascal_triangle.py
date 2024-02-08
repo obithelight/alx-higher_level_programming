3b9bb3819c87aadd880f0bc83cf788702a6400d8
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-08 20:39:57 +0800

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,7 +3,10 @@
 
 
 def pascal_triangle(n):
-    ''' Returns a list of lists of integers '''
+    '''
+    Returns a list of lists of integers
+    representing the Pascal's triangle of n
+    '''
 
     if (n <= 0):
         return []
