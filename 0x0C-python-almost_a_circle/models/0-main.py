30ceed1d0e6a34577b92ba20810c7434aa734826
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-13 10:08:48 +0800

    base class

--- 0x0C-python-almost_a_circle/models/0-main.py
+++ 0x0C-python-almost_a_circle/models/0-main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 """ 0-main """
+import sys
+import os
+
+
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Add the parent directory to the Python path
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
+# Now you can import the module
 from models.base import Base
 
 if __name__ == "__main__":
