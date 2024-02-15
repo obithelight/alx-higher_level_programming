27aea4848707baeb6fbf9d96cece07bb8a07f8f9
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-15 17:18:06 +0800

    implement *args and **kwargs to square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,29 +31,21 @@ class Square(Rectangle):
     def update(self, *args, **kwargs):
         ''' assigns key word attributes to square class '''
         if args:
-            for index, arg in enumerate(args):
-                if index == 0:
-                    self.id = arg
-                elif index == 1:
-                    self.width = arg
-                    self.height = arg
-                elif index == 2:
-                    self.x == arg
-                elif index == 3:
-                    self.y = arg
-                else:
-                    continue
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
 
-        elif len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                elif key == "size":
-                    self.width = value
-                    self.height = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
-                else:
-                    continue
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
