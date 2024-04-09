8c2c552d7bcb173ffd6b92b1b1190128f49f077f
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-09 03:49:19 +0100

    script that prints a message depending on the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const argsLength = process.argv.length;
 
 if (argsLength === 2) {
   console.log('No argument');
-}else if (argsLength === 3) {
+} else if (argsLength === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
