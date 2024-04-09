007670ef5b19be276719ed81fd5179ca33dea49d
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-09 06:38:41 +0100

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   const size = Number(process.argv[2]);
   for (let i = 0; i < size; i++) {
     let line = '';
-    for (let j = 0; j < size; j++ ) {
+    for (let j = 0; j < size; j++) {
       line += 'X';
     }
     console.log(line);
