58634e9659fcc98f04ad61a2e82d76fd4317d1d7
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-13 13:52:09 +0100

    inserted comments to make code more readable

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
 USE hbtn_0c_0
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+Import in hbtn_0c_0 database this table dump: download
+
+Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,4 @@
+-- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 SELECT city, AVG(value) AS avg_temp
 FROM hbtn_0c_0.temperatures
 GROUP BY city
