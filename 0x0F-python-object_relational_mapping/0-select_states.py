844dff5dc85bea97d052bb8ae8894f245e773813
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-15 02:44:48 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Module that lists all states from mySQL database"""
-import sys
 import MySQLdb
 
-if __name__ == "__main__":
+db = MySQLdb.connect(user="obi", passwd="", db="hbtn_0e_0_usa")
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states")
+states = cursor.fetchall()
 
-  db = MySQLdb.connect(user="obi", passwd="", db="hbtn_0e_0_usa")
-  cursor = db.cursor()
-  cursor.execute("SELECT * FROM states")
-  states = cursor.fetchall()
-
-  for state in states:
+for state in states:
     print(state)
 
-  cursor.close()
-  db.close()
+cursor.close()
+db.close()
