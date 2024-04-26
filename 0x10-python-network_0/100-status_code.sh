56d33454cd5aace841fa9c7d975119a707774eac
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-26 06:08:17 -0700

    sends a request to a URL, and displays only the status code of the response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -o /dev/null -sI -w "%{http_code}" $1 ;
+curl -sI $1 -o /dev/null -w "%{http_code}" ;
