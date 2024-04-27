cee0a3dd0b7270f3a5cc947f81d25b1e00a94449
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-27 04:58:46 -0700

    this script takes in a URL and an email, sends a POST request to the URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 '''takes in a URL and email, sends a POST request to the URL with the email'''
 
-import urllib.response
+import urllib.request
 import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
+    value = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(values)
+    data = urllib.parse.urlencode(value)
     data = data.encode('ascii')  # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
