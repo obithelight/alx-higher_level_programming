e188f076dce8abd0535f67a8c58e41e2660b88e2
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-27 16:14:32 +0100

    script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''script that fetches https://alx-intranet.hbtn.io/status'''
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status',
+                       auth=('status', 'pass'))
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
