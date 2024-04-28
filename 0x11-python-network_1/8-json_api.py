c772704529c0f4c3e6081516ac6a03dd0cd7afb9
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-28 09:15:29 +0100

    script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''takes in a letter, sends POST request to http://0.0.0.0:5000/search_user'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        mydata = {'q': ''}
+    else:
+        mydata = {'q': sys.argv[1]}
+
+    req = requests.post(url, data=mydata)
+
+    try:
+        req = req.json()
+        if req == {}:
+            print("No result")
+        else:
+            print(req)
+
+    except ValueError:
+        print("Not a valid JSON")
