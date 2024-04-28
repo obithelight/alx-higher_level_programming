38c74ee0a4b22a0cf4a14cc943dcdeaa8898100e
Obi Chukwu <kennetho.onu@gmail.com>
2024-04-28 12:04:40 +0100

    script that evaluates candidates applying for a back-end position

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
 
     for commit in commits[:10]:
         sha = commit.get('sha')
-        author = commit['commit']['author']['name']
+        author = commit["commit"]["author"]["name"]
         print(f"{sha}: {author}")
