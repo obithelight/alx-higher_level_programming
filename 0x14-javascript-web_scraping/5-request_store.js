d5f4d8054ba52773e0e8b9bcbadbe4cf10c99612
Obi Chukwu <kennetho.onu@gmail.com>
2024-05-22 01:57:56 +0100

    script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Gets the contents of a webpage and stores it in a file
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // write to file
+    fs.writeFile(file, body, { flag: 'w' }, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
