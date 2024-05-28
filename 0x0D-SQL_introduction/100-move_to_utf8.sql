293995332406969d51312597702a0bd34813bf12
Obi Chukwu <kennetho.onu@gmail.com>
2024-05-28 19:24:48 +0100

    updated advanced task - Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,13 @@
--- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
-USE hbtn_0c_0
-ALTER TABLE first_table
-Import in hbtn_0c_0 database this table dump: download
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
 
-Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Use the hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Convert all tables in the database to utf8mb4 character set and utf8mb4_unicode_ci collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert all existing tables and their columns to utf8mb4 character set and utf8mb4_unicode_ci collation
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- If you have multiple tables, repeat the ALTER TABLE command for each table
+-- ALTER TABLE another_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
